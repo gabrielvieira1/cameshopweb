cc41a456274e798d29b1030db71388761d669349
Gabriel Vieira <gabrielvieira1@protonmail.com>
2021-09-28 04:28:20 -0300

    Refactor and add new url

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import axios from "axios";
-import api from '../../services/api';
 import smartphoneImg from "../../assets/Images/smartphoneImg.png";
 import { cadastrarProduto } from "../../services/cadastrarProduto";
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:44390/"
+    baseURL: "https://cameshop.azurewebsites.net/"
 });
 
 export default api;
